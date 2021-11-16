11fafb807a08b0ba3a3d2a4e15fdebb23f76e578
Jacob Skoog <jacob.skoog@protonmail.com>
2021-11-16 15:18:30 +0100

    dev branch working on version 1.0.7. Planning to make the version 1.1.0 when I feel satisfied enough with functionality

--- flip-tile.js
+++ flip-tile.js
@@ -21,7 +21,7 @@
  * ---- used to style a hidden card
  *
  * @author Jacob Skoog
- * @version 1.0.6
+ * @version 1.0.7
  */
 
 //const lnuImg = new URL('pictures/lnu-symbol.png', import.meta.url).href
